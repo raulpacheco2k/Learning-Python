beefcbb708456b0c94d1a3f5330807d10e89bfbc
Raul Pacheco <eu@raulpacheco.com.br>
2021-03-21 22:52:35 -0300

    Add validation

--- Sequential Structure/challenge_12.py
+++ Sequential Structure/challenge_12.py
@@ -1,6 +1,16 @@
 # Taking as input data the height of a person, build an algorithm that calculates your ideal weight, using the following formula: (72.7*altura) - 58
 
-height = int(input('How tall are you in centimeters? '))
+def get_height():
+    while True:
+        try:
+            height = int(input('How tall are you in centimeters? '))
+        except ValueError:
+            print('Please, type a valid number.. Try again.')
+        else:
+            return height
+
+
+height = get_height()
 
 weight = (72.7 * (height/100)) - 58
 
