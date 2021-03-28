254f6673b01c2eb5757802b9b128fe1fa4d2e69c
Raul Pacheco <eu@raulpacheco.com.br>
2021-03-28 12:59:49 -0300

    Decision structure challenge 2

---
+++ Decision Structure/challenge_2.py
@@ -0,0 +1,8 @@
+# Make a Program that asks for two numbers and prints out the larger of them.
+
+number = float(input("Type a number: "))
+
+if number > 0:
+    print("The number is positive, that is, greater than zero.")
+else:
+    print("The number is negative, that is, less than zero.")
