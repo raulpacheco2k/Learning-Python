aaae71c7ebc85e039cda629131470500db7c11c4
Raul Pacheco <eu@raulpacheco.com.br>
2021-03-28 12:57:47 -0300

    Decision structure challenge 1

---
+++ Decision Structure/challenge_1.py
@@ -0,0 +1,9 @@
+# Make a Program that asks for two numbers and prints out the larger of them.
+
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+if first_number > second_number:
+    print("The first number is larger than the second.")
+else:
+    print("The second number is larger than the first.")
